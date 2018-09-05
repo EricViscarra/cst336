d91382abc12c9c4070cc41d9d0d88da1c1946efb
Eric Orozco Viscarra <eorozco-viscarra@csumb.edu>
2018-09-05 19:46:37 +0000

    adding index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html><body>
+<?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+
+echo "Eric Orozco Viscarra's workspace page";
+
+?>
+</body>
+</html>
