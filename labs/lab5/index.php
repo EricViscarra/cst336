d367cd92d0f5d662ea2e0ba356e705bd365ea931
Eric Orozco Viscarra <eorozco-viscarra@csumb.edu>
2018-10-22 20:18:36 +0000

    buddy badge

--- labs/lab5/index.php
+++ labs/lab5/index.php
@@ -52,6 +52,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
         <title>Products Page</title>
     </head>
     <body>
@@ -88,5 +89,19 @@
             <?php displayResults(); ?>
         </div>
     </div>
+    
+    <footer>
+            
+            <hr>
+            
+            CST336 Internet Programming. 2018 &copy; Eric Orozco Viscarra <br />
+            
+            <strong>Disclaimer:</strong> The information in this website is ficticious. <br /> It is used for academic purposes only. <br />
+            <br />
+            
+            <img src="../../img/csumb.png" alt="CSUMB logo" />
+            <img src="../../img/buddy.png" alt="Buddy Ribbon" />
+            
+        </footer>
     </body>
 </html>
