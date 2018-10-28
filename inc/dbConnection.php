3af3497819284802e7fee40bc045fbcabb0eed1f
Eric Orozco Viscarra <eorozco-viscarra@csumb.edu>
2018-10-28 05:56:17 +0000

    Lab 6 done

--- inc/dbConnection.php
+++ inc/dbConnection.php
@@ -2,10 +2,20 @@
 function startConnection($dbName = "ottermart") {
     //Creating database connection
     $host = "localhost";
-    //$dbname = "ottermart";
     $username = "root";
     $password = "";
-    $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    
+    //when connecting from Heroku
+     if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbname = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    } 
+
+
+    $dbConn = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
     $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
     return $dbConn;
--- labs/lab6/index.php
+++ labs/lab6/index.php
@@ -1,55 +1,109 @@
 <?php
 include "../../inc/dbConnection.php";
 $dbConn = startConnection("ottermart");
+
 function displayCategories() {
     global $dbConn;
-    $sql = "SELECT * FROM om_category ORDER BY catName";
+    
+    $sql = "SELECT catId, catName FROM om_category ORDER BY catName";
+    
     $stmt = $dbConn->prepare($sql);
     $stmt->execute();
     $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
     echo "<br />";
+    
     foreach ($records as $record) {
-        echo "<option>".$record['catName'] . "</option>";
+        echo "<option value='".$record["catId"]."' >" . $record["catName"]. "</option>";
     }
 }
-function filterProducts() {
+function displaySearchResults() {
     global $dbConn;
-    $product = $_GET['productName'];
-    //$sql = "SELECT * FROM om_product
-    //        WHERE productName LIKE '%$product%' OR productDescription LIKE '%$product%";
-    $sql = "SELECT * FROM om_product
-            WHERE productName LIKE :product";
     
-    $namedParameters = array();
-    
-    $namedParameters[':product'] = "%$products%";
-    
-    $stmt = $dbConn->prepare($sql);
-    $stmt->execute($namedParameters);
-    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    print_r($records);
+    if (isset($_GET["s"])) {
+        
+        echo "<h3>Products Found: </h3>";
+        
+        $namedParameters = array();
+        
+        $sql = "SELECT * FROM om_product WHERE 1";
+        
+        if (!empty($_GET['product'])) {
+            $sql .= " AND productName LIKE :productName OR productDescription LIKE :productName";
+            $namedParameters[":productName"] = "%" . $_GET['product'] . "%";
+        }
+        
+        if (!empty($_GET['category'])) {
+            $sql .= " AND catId = :categoryId";
+            $namedParameters[":productName"] = "%" . $_GET['product'] . "%";
+        }
+        
+        if (!empty($_GET['priceFrom'])) {
+            $sql .= " AND price >= :priceFrom";
+            $namedParameters[":priceFrom"] = $_GET['priceFrom'];
+        }
+        
+        if (!empty($_GET['priceTo'])) {
+            $sql .= " AND price <= :priceTo";
+            $namedParameters[":priceTo"] = $_GET['priceTo'];
+        }
+        
+        if (!empty($_GET['orderBy'])) {
+            if ($_GET['orderBy'] == "price") {
+                
+                $sql .= " ORDER BY price";
+            }
+            else {
+                $sql .= " ORDER BY productName";
+            }
+            
+        }
+        
+        $stmt = $dbConn->prepare($sql);
+        $stmt->execute($namedParameters);
+        $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($records as $record) {
+            
+            echo "<a href=\"purchaseHistory.php?productId=".$record["productId"]. "\"> History </a>";
+            echo $record["productName"] . " " . $record["productDescription"] . " $" . $record["price"] . "<br /><br />";
+        }
+    }
 }
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <title> Lab 6: Ottermart Product Search </title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
-        <h1> Ottermart </h1>
-        <h2> Product Search</h2></h1>
+        <h1> Ottermart Product Search</h1>
         
         <form>
             
-            Product: <input type="text" name="productName" placeholder="Product Keyword" />
+            Product: <input type="text" name="product" placeholder="Product Keyword" />
             
             <br />
             
-            Category: <select name="category">
-                <option value=""> Select one </option>
+            Category: 
+                <select name="category">
+                    <option value=""> Select one </option>
                 <?=displayCategories()?>
-            </select>
-            <input type="submit" name="submit" value="Search!" />
+                </select>
+            <br />
+            Price: From <input type="text" name="priceFrom" size="7"/>
+                    to <input type="text" name="priceTo" size="7"/>
+            <br />
+            Order result by:
+            <br />
+            
+            <input type = "radio" name="orderBy" value="price"/> Price <br />
+            <input type = "radio" name="orderBy" value="name"/> Name <br />
+            <br />
+            <input type="submit" value="Search!" name="s"/>
         </form>
+        
+        <br />
+        <hr>
+        <?= displaySearchResults() ?>
     </body>
 </html>
