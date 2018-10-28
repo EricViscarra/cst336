e5b168914b8ee07774ffeb4415688f2e00a7983e
Eric Orozco Viscarra <eorozco-viscarra@csumb.edu>
2018-10-28 06:36:43 +0000

    typo

--- inc/dbConnection.php
+++ inc/dbConnection.php
@@ -9,11 +9,10 @@ function startConnection($dbName = "ottermart") {
      if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
         $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
         $host = $url["host"];
-        $dbname = "heroku_621330f66ea5ba3";
-        $username = "bc241451b56c3b";
-        $password = "bf09f02f";
+        $dbName = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
     } 
-
 
     $dbConn = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
     $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
