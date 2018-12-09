8fa095fc5f359a0a384d527950b56e84f9961253
Eric Orozco Viscarra <eorozco-viscarra@csumb.edu>
2018-12-09 07:38:59 +0000

    buddy badge added

--- labs/lab10/inc/footer.php
+++ labs/lab10/inc/footer.php
@@ -2,8 +2,9 @@
         <footer>
             
             Disclaimer: The information on this site is fake.<br>
-            &copy; Lara 2019
-            
+            &copy; Eric Orozco Viscarra 2019 <br>
+            <img src="../../img/csumb.png" alt="CSUMB logo" />
+            <img src="../../img/buddy.png" alt="Buddy Ribbon" />
         </footer>
         
         
