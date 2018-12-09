ee7b806ad10a8766a42cea09089560ff39900492
Eric Orozco Viscarra <eorozco-viscarra@csumb.edu>
2018-12-09 07:37:16 +0000

    Adding lab 10 complete

--- labs/lab10/index.php
+++ labs/lab10/index.php
@@ -5,21 +5,93 @@
         
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>   
+	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>   
+        
         <style>
             body {
                 text-align: center;
+            }
+            
+            .carousel.slide {
+                height : 30%;
+                width : 30%;
+            }
+            
+            .carousel {
+                margin: auto;
             }
         </style>
    
     </head>
     <body>
         
-	<!--Add main menu here -->
-        
-        
-        <div class="jumbotron">
-          <h1> CSUMB Animal Shelter</h1>
-          <h2> The "official" animal adoption website of CSUMB </h2>
+	  <?php 
+	    include 'inc/header.php';
+	    
+	  ?>
+        <!-- Display Carousel here  -->
+        <?php
+            include '../../inc/dbConnection.php';
+            $dbConn = startConnection("c9");    
+    
+            $sql = "SELECT pictureURL FROM pets ORDER BY name ASC";
+    
+            $stmt = $dbConn->prepare($sql);
+            $stmt->execute();
+            $records = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+            $imageURLs = array();
+            
+            foreach ($records as $image) {
+                array_push($imageURLs, $image["pictureURL"]);
+            }
+        ?>
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="img/<?=$imageURLs[0]?>" alt="First slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="img/<?=$imageURLs[1]?>" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="img/<?=$imageURLs[2]?>" alt="Third slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="img/<?=$imageURLs[3]?>" alt="Fourth slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="img/<?=$imageURLs[4]?>" alt="Fifth slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="img/<?=$imageURLs[5]?>" alt="Sixth slide">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
         </div>
+        <br>
+        <a class="btn btn-outline-success" href="pets.php" role="button">Adopt Now</a>
+        <br><br><br>
+        <?php
+        include 'inc/footer.php';
+        
+        ?>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+        </body>
+
+</html>
